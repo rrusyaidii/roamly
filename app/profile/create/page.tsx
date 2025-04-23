a1e799aa51c7fc59203a618ccc931214dea40981
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2025-04-23 14:54:09 +0800

    added schema

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,21 +1,15 @@
 import FormInput from "@/components/form/FormInput";
 import { SubmitButton } from "@/components/form/Buttons";
 import FormContainer from "@/components/form/FormContainer";
-
-const createProfileAction = async (prevState: any, formData: FormData) => {
-  "use server";
-  const firstName = formData.get("firstName") as string;
-  if (firstName !== "shakeAndBake") return { message: "there was an error..." };
-  return { message: "Profile Created" };
-};
+import { createProfileAction } from "@/utils/actions";
 
 function CreateProfile() {
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
-      <div className="border p-8 rounded-md max-w-lg">
+      <div className="border p-8 rounded-md  ">
         <FormContainer action={createProfileAction}>
-          <div className="grid gap-4 mt-4 ">
+          <div className="grid md:grid-cols-2 gap-4 mt-4 ">
             <FormInput type="text" name="firstName" label="First Name" />
             <FormInput type="text" name="lastName" label="Last Name" />
             <FormInput type="text" name="username" label="Username" />
