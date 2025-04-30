6844eeb5d629c397b67a4ba0fb2f6135d6652d76
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2025-04-30 17:20:08 +0800

    schema and db

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,18 +1,55 @@
 "use server";
 
 import { profileSchema } from "./schema";
+import db from "./db";
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
+    const user = await currentUser();
+    //if no user throw error
+    if (!user) throw new Error("Please login to create a profile");
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: "Profile Created" };
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
-    console.log(error);
-    return { message: "There was an error..." };
+    return {
+      message: error instanceof Error ? error.message : "An error occured",
+    };
   }
+  redirect("/");
+};
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+  return profile?.profileImage;
 };
